19400b6c2e4a7159279fe9e15752bcf23c75d257
yufei <sisufointernational@gmail.com>
2013-11-01 11:48:22 +0800

    Make the comment of normal variabel to be one line without multiline by default

--- VVDocumenter-Xcode/Commenter/VVVariableCommenter.m
+++ VVDocumenter-Xcode/Commenter/VVVariableCommenter.m
@@ -7,7 +7,27 @@
 //
 
 #import "VVVariableCommenter.h"
+#import "VVDocumenterSetting.h"
 
 @implementation VVVariableCommenter
+
+-(NSString *) document
+{
+    NSString * comment = [NSString stringWithFormat:@"%@%@%@%@%@",
+                          [self startCommentWithWrapped:NO],
+                          [self argumentsComment],
+                          [self returnComment],
+                          [self sinceComment],
+                          [self endComment]];
+    
+    // The last line of the comment should be adjacent to the next line of code,
+    // back off the newline from the last comment component.
+    if ([[VVDocumenterSetting defaultSetting] prefixWithSlashes]) {
+        return [comment stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    } else {
+        return comment;
+    }
+}
+
 
 @end
